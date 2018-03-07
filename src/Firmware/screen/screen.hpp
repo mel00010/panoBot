102365421146a2387bdb4e3404695b47f9eea5b5
John Drake <jdrake@uoregon.edu>
2018-03-06 19:43:39 -0800

    Adding screen files

---
+++ src/Firmware/screen/screen.hpp
@@ -0,0 +1,29 @@
+
+#include <string>
+
+
+
+
+
+
+class Screen{
+public:
+	Screen();
+	~Screen();
+	//erases entire screen
+	void eraseScreen();
+	//writes a string to screen 
+	void write(const std::string  &input);
+	//position to write to in "screen"
+	void moveCursor(const size_t &x, const size_t y);
+	//gets current cursor position
+	std::pair<size_t, size_t> getCursorPosition();
+	//overwriting in a certain position
+	void update(const std::string &input, const size_t x, const size_t y);
+	//erases at a certain position rather than the entire screen
+	void erasePosition(const size_t x, const size_t y);
+
+};
+
+
+
---
+++ src/Firmware/screen/screenmain.cpp
@@ -0,0 +1,44 @@
+#include <string>
+#include <iostream>
+#include <cstdlib>
+#include "screen.hpp"
+
+
+
+
+Screen::Screen(){
+
+}
+
+Screen::~Screen(){
+
+}
+
+//erases the screen
+void Screen::eraseScreen(){
+
+}
+
+void Screen::write(const std::string  &input){
+
+}
+
+void Screen::moveCursor(const size_t &x, const size_t y){
+
+}
+
+std::pair<size_t, size_t> Screen::getCursorPosition(){
+
+}
+
+
+void Screen::update(const std::string &input, const size_t x, const size_t y){
+
+}
+
+
+void Screen::erasePosition(const size_t x, const size_t y){
+
+}
+
+
